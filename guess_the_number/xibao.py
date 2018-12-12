53f9db07b5f6e6212cc2e577ce2bde05ccebbf3b
xibaochat <42967814+xibaochat@users.noreply.github.com>
2018-12-12 22:50:25 +0100

    Create xibao.py

---
+++ guess_the_number/xibao.py
@@ -0,0 +1,22 @@
+import random
+
+def ask_input_to_user(msg):
+    try:
+        number = int(input(msg))
+    except:
+        return ask_input_to_user('Please enter an integer: ')
+    return number
+
+def guess_the_number():
+    random_num = random.randint (0, 10000) 
+
+    number_given_by_user = ask_input_to_user("Give me a number: ")
+    
+    while(random_num != number_given_by_user):
+        if random_num < number_given_by_user:
+            number_given_by_user = ask_input_to_user('Too high, try again: ')
+        elif random_num > number_given_by_user:
+            number_given_by_user = ask_input_to_user('Too low, try again: ')
+    print('Felicitation!')
+
+guess_the_number()
