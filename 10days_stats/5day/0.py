43db16e67b12a0a5dcac53a30de622a5447c9f06
Baobe <wmmxxbb@gmail.com>
2018-12-25 16:53:55 -0500

    first exercise of 5th days of python of 10days challenge

---
+++ 10days_stats/5day/0.py
@@ -0,0 +1,17 @@
+import math
+
+def factorial(n):
+    if n == 0:
+        return 1
+    return n* factorial(n-1)
+
+def calculate_value(m, n):
+    value =  (math.exp(-1 * m) * (m**n)) / factorial(n)
+    return round(value, 3)
+
+m =float(input())
+n = float(input())
+
+print(calculate_value(m, n))
+
+                        
