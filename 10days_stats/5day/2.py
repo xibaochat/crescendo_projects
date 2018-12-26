7ddf763197ac46304d51713ee66e6f9bba55c7fc
Baobe <wmmxxbb@gmail.com>
2018-12-25 20:13:42 -0500

    the third exercise of 5th day of 10day challenge

--- 10days_stats/5day/2.py
+++ 10days_stats/5day/2.py
@@ -1,66 +1,21 @@
-
 import math
-def cumulative_possibility(x):
+def cumu_pro(x, mean, sd):
+    return 0.5 * (1 + math.erf((x - mean) / (sd *  2**0.5)))
 
-    return (1/2 * (1 + math.erf(x - normal_distribution_mean)) / (standard_deviation *  (2**1/2)))
+
+mean, sd = list(map(float, input().split()))
+
+time_a = float(input())
+
+time_b, time_c = list(map(float, input().split()))
+
+cumu_pro_1 = round(cumu_pro(time_a, mean, sd), 3)
+cumu_pro_2 = round((cumu_pro(time_c, mean, sd) - cumu_pro(time_b, mean, sd)), 3)
+
+print(cumu_pro_1, cumu_pro_2, sep = '\n')
 
 
-normal_distribution_mean, standard_deviation = list(map(float, input().split()))
 
-assemble_time_a = float(input())
 
-assemble_time_b, assemble_time_c = list(map(float, input().split()))
 
-cumulative_possibility_a = round(cumulative_possibility(assemble_time_b), 3)
-cumulative_possibility_b = round((cumulative_possibility(assemble_time_c) - cumulative_possibility(assemble_time_b)), 3)
-
-print(cumulative_possibility_a, cumulative_possibility_b)
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
